70df942f542e1f3ef9bf62a6b008202121214216
Sharayu Yeole <sharayuyeole38@gmail.com>
2025-09-29 17:56:03 +0530

    Push

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let timer;
-let seconds =0, minutes=0, hours =0;
+let millisecond = 0, seconds =0, minutes=0, hours =0;
 let isRunning = false;
 
 function updateDisplay(){
@@ -7,25 +7,30 @@ function updateDisplay(){
     let h = hours < 10 ? "0" + hours : hours;
     let m = minutes < 10 ? "0" + minutes : minutes;
     let s = seconds < 10 ? "0" + seconds : seconds;
+    let ms = millisecond < 10 ? "0" + millisecond : millisecond;
 
-    display.innerText = `${h}:${m}:${s}`;
+    display.innerText = `${h}:${m}:${s}:${ms}`;
 }
 
 function start(){
     if(!isRunning){
         isRunning = true;
         timer = setInterval(() => {
-            seconds++;
-            if(seconds >= 60){
-                seconds = 0;
-                minutes++;
-                if(minutes >= 60){
-                    minutes = 0;
-                    hours++;
+            millisecond++;
+            if(millisecond >= 100){
+                millisecond = 0;
+                seconds++;
+                if(second >= 60){
+                    seconds = 0;
+                    minutes++;
+                    if(minutes >= 60){
+                        minutes = 0;
+                        hours++;
+                    }
                 }
             }
             updateDisplay();
-        }, 1000);
+        }, 10);
     }
 }
 
@@ -37,6 +42,7 @@ function stop(){
 function reset(){
     clearInterval(timer);
     isRunning = false;
+    millisecond = 0;
     seconds = 0;
     minutes = 0;
     hours = 0;
